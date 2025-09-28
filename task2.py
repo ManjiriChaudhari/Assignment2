f31e34b8740291a5895e3e06e455f645aa619557
ManjiriChaudhari <166192801+ManjiriChaudhari@users.noreply.github.com>
2025-09-28 18:08:12 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Task 2: Sum of Integers from 1 to 50
+
+# Initialize sum variable
+total_sum = 0
+
+# Iterate over numbers from 1 to 50
+for i in range(1, 51):
+    total_sum += i
+
+# Display the final sum
+print("The sum of integers from 1 to 50 is:", total_sum)
